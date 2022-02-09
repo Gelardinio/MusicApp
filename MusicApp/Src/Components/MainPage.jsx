7222dd8fbded2f0a4fbc19ee2c1de56841276e89
Gelardinio <geraldzliu@outlook.com>
2022-02-08 22:37:28 -0500

    Added location markers and permissions

--- MusicApp/Src/Components/MainPage.jsx
+++ MusicApp/Src/Components/MainPage.jsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import React from "react";
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Logout from './Logout';
+import * as Location from 'expo-location';
 
 const onSubmit = (values) => {
     console.log(values);
@@ -21,9 +22,38 @@ const styles = StyleSheet.create({
 });
 
 const MainPage = () => {
+
+    const [location, setLocation] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+    const [longitude, setLongitude] = React.useState(0);
+    const [latitude, setLatitude] = React.useState(0);
+
+    React.useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        if (location) {
+          setLatitude(location.coords.latitude)
+          setLongitude(location.coords.longitude)
+        }
+      })();
+    }, []);
+
     return (
         <>
-            <MapView style={styles.map} />
+            <MapView style={styles.map}>
+                <MapView.Marker
+                    coordinate={{latitude: latitude,
+                    longitude: longitude}}
+                    title={"title"}
+                    description={"description"}
+                />
+            </MapView>
             <Logout />
         </>
     )
